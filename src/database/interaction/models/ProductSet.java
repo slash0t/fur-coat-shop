9dab494e3ac207247d22c0965074591366e42a5f
Artem <minenion2@gmail.com>
2024-12-09 22:39:57 +0300

    Fix returning value

--- src/database/interaction/models/ProductSet.java
+++ src/database/interaction/models/ProductSet.java
@@ -5,7 +5,7 @@ import database.data.ProductSetData;
 public abstract class ProductSet {
     public abstract void create(ProductSetData productSet);
 
-    public abstract ProductSetData getWithCart(String cartId);
+    public abstract Iterable<ProductSetData> getWithCart(String cartId);
     public abstract ProductSetData getWithCartProductSize(String cartId, String productId, String size);
 
     public abstract void changeCountById(String id, int count);
